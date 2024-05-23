93b97675eda1f4ddf1d5daf3c504cd3c6dcde816
Egor Savenko <smahelzed@gmail.com>
2024-05-23 22:28:45 +0300

    ADD: router config, main packages, root component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import Layout from "@/layout/Layout.vue";
 </script>
 
 <template>
-
+<layout>
+  <router-view />
+</layout>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
+import router from '@/router/index'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
+import '@/assets/base.css'
 
 const app = createApp(App)
 
+app.use(router)
 app.use(createPinia())
 
 app.mount('#app')
