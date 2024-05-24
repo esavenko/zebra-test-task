8507ca35c8a5602a6c9e76d5afec7f3c4dfc053f
Egor Savenko <smahelzed@gmail.com>
2024-05-24 20:13:07 +0300

    REFACTOR: change component structure

--- src/layouts/AppHeader.vue
+++ src/layouts/AppHeader.vue
@@ -3,21 +3,29 @@ import MainLogo from '../../public/logos/MainLogo.vue';
 </script>
 
 <template>
-<header class="header">
-  <div class="header__container">
-    <div class="header__logo">
-      <main-logo
-        :width="152"
-        :height="48"
-      />
+  <header class="header">
+    <div class="header__container">
+      <div class="header__wrap">
+        <div class="header__logo">
+          <main-logo
+              :width="152"
+              :height="48"
+          />
+        </div>
+      </div>
     </div>
-  </div>
-</header>
+  </header>
 </template>
 
 <style scoped>
 .header__container {
   max-width: var(--container-val);
   margin: 0 auto;
+}
+
+.header__wrap {
+  display: flex;
+  align-items: center;
+  height: 123px;
 }
 </style>
--- src/pages/AppPage.vue
+++ src/pages/AppPage.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-
+import PostHeader from "@/components/PostHeader.vue";
+import NewsList from "@/components/NewsList.vue";
 </script>
 
 <template>
-
+<main class="content">
+  <post-header />
+  <news-list />
+</main>
 </template>
 
 <style scoped>
