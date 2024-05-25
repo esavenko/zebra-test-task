8d39861acfc6611923167dff2f426050ac1e1240
Egor Savenko <smahelzed@gmail.com>
2024-05-25 11:29:05 +0300

    added try/catch block for query

--- src/stores/news.store.ts
+++ src/stores/news.store.ts
@@ -1,20 +1,25 @@
 import { defineStore } from 'pinia';
-import axios from "axios";
+import axios from 'axios';
+import type { INews } from '@/types/interfaces';
 
 import { ref } from 'vue';
 
 const URL = 'http://flems.github.io/test/api/news/';
 
 export const useNewsStore = defineStore('newsStore', () => {
-    const news = ref([]);
+    const news = ref<INews[]>([]);
 
     const getAllNews = async () => {
-        const res = await axios.get(URL)
-        news.value = res.data.items
+        try {
+            const res = await axios.get(URL);
+            news.value = res.data.items;
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return {
         news,
         getAllNews,
-    }
+    };
 });
