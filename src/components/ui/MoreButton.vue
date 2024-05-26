0f8976942f11f825550b3c527c085421b7e79869
Egor Savenko <smahelzed@gmail.com>
2024-05-26 19:10:50 +0300

    ADD: Ui component button and Loader

---
+++ src/components/ui/MoreButton.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {useNewsStore} from '@/stores/news.store';
+
+const newsStore = useNewsStore();
+</script>
+
+<template>
+  <div
+      v-if="newsStore.hasMoreNews"
+      class="more-button"
+  >
+    <button
+        class="more-button__btn"
+        @click="newsStore.getMoreNews(2)"
+    >
+      Загрузить еще
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.more-button {
+  display: flex;
+  justify-content: center;
+  margin: 72px auto;
+}
+
+.more-button__btn {
+  width: 203px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #002DBF;
+  padding: 16px 32px;
+  border: 1px solid #002DBF;
+  border-radius: 5px;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
---
+++ src/layouts/Loader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="preloader-spinner">
+    <div class="loader">
+      <div class="loader-wheel"/>
+      <div class="loader-text"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.preloader-spinner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #ecf6ff;
+  z-index: 21;
+  display: flex;
+  justify-content: center;
+}
+
+.spinner-container .spinner-symbol rect {
+  height: 20px;
+  width: 20px;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(806deg);
+  }
+}
+
+.loader {
+  width: 50px;
+  margin: auto;
+}
+
+.loader-wheel {
+  animation: spin 1.2s infinite linear;
+  border: 2px solid #4bc6fb;
+  border-left: 4px solid #fff;
+  border-radius: 50%;
+  height: 50px;
+  margin-bottom: 10px;
+  width: 50px;
+}
+
+.loader-text {
+  color: #fff;
+}
+
+.loader-text:after {
+  animation: load 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
