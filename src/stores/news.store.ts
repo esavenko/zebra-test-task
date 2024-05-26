fef259193dd9703070317a557b1f087f8a402e8c
Egor Savenko <smahelzed@gmail.com>
2024-05-26 19:11:45 +0300

    ADD: new store func

--- src/stores/news.store.ts
+++ src/stores/news.store.ts
@@ -1,25 +1,45 @@
-import { defineStore } from 'pinia';
+import {defineStore} from 'pinia';
 import axios from 'axios';
-import type { INews } from '@/types/interfaces';
+import type {INews} from '@/types/interfaces';
 
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 const URL = 'http://flems.github.io/test/api/news/';
 
 export const useNewsStore = defineStore('newsStore', () => {
     const news = ref<INews[]>([]);
+    const hasMoreNews = ref(true);
+    const isLoading = ref(false);
 
     const getAllNews = async () => {
         try {
+            isLoading.value = true;
             const res = await axios.get(URL);
             news.value = res.data.items;
+            isLoading.value = false;
         } catch (error) {
             console.error(error);
         }
     };
 
+    const getMoreNews = async (page: number) => {
+        try {
+            isLoading.value = true;
+            const res = await axios.get(`${URL}/${page}`)
+            news.value.push(...res.data.items);
+            hasMoreNews.value = false;
+            isLoading.value = false;
+        } catch (error) {
+            console.error(error);
+            hasMoreNews.value = false;
+        }
+    };
+
     return {
         news,
+        hasMoreNews,
+        isLoading,
         getAllNews,
+        getMoreNews,
     };
 });
